2410d8c9846f13c501b90c929f299deecdcb6356
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-28 23:45:35 +0000

    Created read file and vaiables with arrays, multi-d and single-d

---
+++ deptsalaries01.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+//single-d array - valid
+var departmentID = [];
+var departments = [];
+
+// multi-d arrays - not valid need to push sub arrays inside
+var employeeID = [];
+var employeeName = [];
+var salaries = [];
