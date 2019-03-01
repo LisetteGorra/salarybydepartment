36b475d1e049dae46972b674f820cc41b0731727
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-01 01:34:01 +0000

    Populated multi-d arrays with sub arrays

--- deptsalaries01.js
+++ deptsalaries01.js
@@ -8,3 +8,28 @@ var departments = [];
 var employeeID = [];
 var employeeName = [];
 var salaries = [];
+
+//Proccess 'departments.txt' file
+
+fs.readFile('departments.txt', 'utf8', function(error, data){
+    if (error) throw error;
+    
+    var cleanDeptData = data.replace(/INSERT INTO `departments` Values /g, "");
+    var deptDataArray = cleanDeptData.split('\n');
+   
+   for(var i = 0; i < deptDataArray.length; i++){
+       //populate single-d arrays
+       departmentID.push(deptDataArray[i].slice(2,6));
+       departments.push(deptDataArray[i].slice(9,-3));
+       //populate multi-d arrays 
+       employeeID.push([]);
+       employeeName.push([]);
+       salaries.push([]);
+       
+       
+   }
+   console.log(salaries);
+//   console.log(departments);
+  
+   
+})
